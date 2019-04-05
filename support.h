e95e51c5e1e727b7155e6b044934a70462e7ffaf
Aswin Ajayan <aswinp2008@gmail.com>
2019-04-06 00:37:45 +0530

    initial commit. supporting macros

--- support.h
+++ support.h
@@ -15,4 +15,24 @@
  *
  * =====================================================================================
  */
+#ifndef __SUPPORT_H__
+#define __SUPPORT_H__
 
+#include <gtk/gtk.h>
+
+/* Convenience macros for obtaining objects from UI file */
+#define CH_GET_OBJECT( builder, name, type, data ) \
+    data->name = type( gtk_builder_get_object( builder, #name ) )
+#define CH_GET_WIDGET( builder, name, data ) \
+    CH_GET_OBJECT( builder, name, GTK_WIDGET, data )
+
+/* Main data structure definition */
+typedef struct _ChData ChData;
+struct _ChData
+{
+    /* Widgets */
+    GtkWidget *main_window;  /* Main application window */
+    GtkWidget *chart_area;   /* Chart drawing area */
+};
+
+#endif /* __SUPPORT_H__ */
