f25710e622d1ad79d8dc48574d3e65c304152dc8
Aswin P Ajayan <aswinp2008@gmail.com>
2019-04-06 03:38:33 +0530

    modified UDP_server program to use pthreads

---
+++ UDP_server.c
@@ -0,0 +1,156 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  UDP_server.c
+ *
+ *    Description:  Code to create a UDP server built on top of/ by  https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=3&ved=2ahUKEwjC9YiX0ZPhAhUJ8HMBHYzIDeQQFjACegQIBBAB&url=https%3A%2F%2Fwww.cs.cmu.edu%2Fafs%2Fcs%2Facademic%2Fclass%2F15213-f99%2Fwww%2Fclass26%2Fudpserver.c&usg=AOvVaw1ZwUkY8jZDhz-iq9Xp8VEG
+ *
+ *        Version:  1.0
+ *        Created:  Thursday 21 March 2019 08:02:35  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *	Original Autthor : cs.cmu.edu
+ *        Modified by:  Aswin P Ajayan (), aswinp2008@gmail.com
+ *   Organization:  iitb
+ *
+ * 
+ * =====================================================================================
+ */
+
+
+/*udp server.c - A simple UDP echo server 
+ * usage: udpserver <port>
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+#define CMD_SIZE 128
+#define BUFSIZE 1024
+
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+/* shared buffered locked through the mutex*/
+
+char in_buf[BUFSIZE]; /* message buf  data from MCU*/
+char out_buf[CMD_SIZE]; /*control messages from UI*/
+pthread_mutex_t lock_buf = PTHREAD_MUTEX_INITIALIZER;
+pthread_t server_t_id;
+
+void* socketThread(void *arg){
+  char recv_buf[BUFSIZE];	
+  int sockfd; /* socket */
+  int portno; /* port to listen on */
+  unsigned int clientlen; /* byte size of client's address */
+  struct sockaddr_in serveraddr; /* server's addr */
+  struct sockaddr_in clientaddr; /* client addr */
+  //struct hostent *hostp; /* client host info */
+  //struct addrinfo *host_info;
+  char *hostaddrp; /* dotted decimal host addr string */
+  int optval; /* flag value for setsockopt */
+  int n; /* message byte size */
+
+  /* 
+   * check command line arguments 
+   */
+  portno = atoi((char*)arg);
+
+  /* 
+   * socket: create the parent socket 
+   */
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+
+  /* setsockopt: Handy debugging trick that lets 
+   * us rerun the server immediately after we kill it; 
+   * otherwise we have to wait about 20 secs. 
+   * Eliminates "ERROR on binding: Address already in use" error. 
+   */
+  optval = 1;
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
+	     (const void *)&optval , sizeof(int));
+
+  /*
+   * build the server's Internet address
+   */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons((unsigned short)portno);
+
+  /* 
+  * bind: associate the parent socket with a port 
+   */
+  if (bind(sockfd, (struct sockaddr *) &serveraddr, 
+	   sizeof(serveraddr)) < 0) 
+    error("ERROR on binding");
+
+  /* 
+   * main loop: wait for a datagram, then echo it
+   */
+  clientlen = sizeof(clientaddr);
+  while (1) {
+
+    /*
+     * recvfrom: receive a UDP datagram from a client
+     */
+    bzero(recv_buf, BUFSIZE);
+    n = recvfrom(sockfd, recv_buf, BUFSIZE, 0,
+		 (struct sockaddr *) &clientaddr, &clientlen);
+    if (n < 0)
+      error("ERROR in recvfrom");
+    
+    pthread_mutex_lock(&lock_buf);
+    bzero(in_buf,BUFSIZE);
+    memcpy(in_buf,recv_buf,strlen(recv_buf)+1);
+    pthread_mutex_unlock(&lock_buf);
+        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+     printf("server received %ld/%d bytes: %s\n", strlen(recv_buf), n, recv_buf);
+ 
+    /* 
+     * sendto: echo the input back to the client 
+     */
+    n = sendto(sockfd, out_buf, strlen(out_buf), 0, 
+	       (struct sockaddr *) &clientaddr, clientlen);
+    if (n < 0) 
+      error("ERROR in sendto");
+  }
+  pthread_exit(NULL);
+}
+
+int main(int argc, char **argv) {
+	
+ //test initialisation of command buffer 
+ 
+	memcpy(out_buf,"TEST_COMMAND",12);
+  /* 
+   * check command line arguments 
+   */
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+	pthread_create(&server_t_id,NULL,&socketThread,argv[1]);
+pthread_join(server_t_id,NULL);
+
+
+}
+
